8eaf8ec8c0920890d419ef2b27864ddc508b83a8
Anthony Mugendi <ngurumugz@gmail.com>
2024-05-11 17:29:43 +0300

    Client configs

--- client/rollup.config.mjs
+++ client/rollup.config.mjs
@@ -2,20 +2,24 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
-import sveltePreprocess from 'svelte-preprocess';
 import hmr from 'rollup-plugin-reloadsite';
 import styles from 'rollup-plugin-styles';
 import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
 import del from 'rollup-plugin-delete';
 import serve from 'rollup-plugin-serve';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import _ from 'lodash';
 
 import fg from 'fast-glob';
-
-import { fileURLToPath } from 'url';
 import path from 'path';
-// import config from 'config';
 
+// import config from 'config';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+// console.log({require});
+import { fileURLToPath } from 'url';
 // expose __filename & __dirname usually not supported in the module system
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,20 +29,34 @@ const production = !process.env.ROLLUP_WATCH;
 
 // fetch all .js files in the src directory
 const srcDir = path.join(__dirname, './src');
-const globPat = path.join(srcDir, '**/*.js');
+const globPat = path.join(srcDir, '**/index.js');
 const entries = fg.sync([globPat], { ignore: ['**/lib/**', '**/vendor/**'] });
 
 // build each
 const script_exports = entries.map((filePath) => {
-  let name = path.relative(srcDir, filePath).split('.').shift();
+  let relPath = path.relative(srcDir, filePath);
 
-  return {
+  let dirName = path.basename(path.dirname(relPath));
+  let name = _.camelCase(dirName);
+  relPath = path.dirname(path.dirname(relPath));
+  let file = path.join('./build', relPath, name + '.js');
+
+  // return
+  console.log({name, file});
+
+  let config = {
     input: filePath,
+
+    // external:['mdsvex'],
+
     output: {
       sourcemap: !production,
-      name: 'app',
+      name: _.camelCase(['cmsFront', name]),
       format: 'iife',
-      file: `./build/${name}${production ? '.min' : ''}.js`,
+      file,
+      // globals: {
+      //   mdsvex,
+      // },
     },
 
     plugins: [
@@ -58,47 +76,29 @@ const script_exports = entries.map((filePath) => {
       json(),
 
       // manage modules
+      nodePolyfills(),
+
       resolve({
         browser: true,
         dedupe: ['svelte'],
         exportConditions: ['svelte'],
       }),
-      commonjs(),
 
-      // process svelte
+      commonjs({ transformMixedEsModules: true }),
+
+      // // process svelte
       svelte({
-        compilerOptions: {
-          // enable run-time checks when not in production
-          dev: !production,
-          // css: 'injected'
-        },
-
-        preprocess: sveltePreprocess({
-          replace: [
-            [
-              /process\.env\.(\w+)/g,
-              (_, prop) => JSON.stringify(process.env[prop]),
-            ],
-          ],
-        }),
+        // these are the defaults. If you want to add more extensions, see https://mdsvex.pngwn.io/docs#extensions
+        extensions: ['.svelte', '.svx'],
       }),
 
       // minify output
       production && terser({ output: { comments: false } }),
 
-      // run server on dev mode
-      !production &&
-      serve({
-        open: true,
-        host: 'localhost',
-        port: 10001,
-        openPage: '/examples/index.html',
-      }),
-
       // hot module reload during
       hmr({
-        dirs: ['./public', '../server/www/static'],
-        filter: '**/cms-front.js',
+        dirs: ['./build'],
+        filter: '**/*.js',
       }),
     ],
 
@@ -107,6 +107,19 @@ const script_exports = entries.map((filePath) => {
       clearScreen: false,
     },
   };
+
+  if (name == 'cmsFront' && !production) {
+    config.plugins.push(
+      serve({
+        open: true,
+        host: 'localhost',
+        port: 10001,
+        openPage: '/examples/index.html',
+      })
+    );
+  }
+
+  return config;
 });
 
 export default script_exports;
